846cc8e718e0af260f06487653e00f15cc5a0c24
wimuweb <wimuweb@gmail.com>
2023-04-13 23:16:33 -0500

    Component compound listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route,NavLink,Navigate } from "react-router-dom"
 
 
 import logo from '../assets/react.svg';
+import ShoppingPage from "../02-component.patterns/pages/ShoppingPage";
 
 const Navigation = () => {
   return (
@@ -11,7 +12,7 @@ const Navigation = () => {
                   <img src={logo} alt="React logo" />
                          <ul>
                   <li>
-                     <NavLink to='/' className={({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                     <NavLink to='/' className={({isActive}) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
                   </li>
                   <li>
                      <NavLink to='/about'className={({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -25,7 +26,7 @@ const Navigation = () => {
               <Routes>
                   <Route path="about" element={<h1>About Page</h1>} />
                   <Route path="users" element={<h1>Users Page</h1>} />
-                  <Route path="/" element={<h1>Home Page</h1>} />
+                  <Route path="/" element={<ShoppingPage/>} />
                   <Route path="*" element={<Navigate to='/' replace/>} />
               </Routes>
 
